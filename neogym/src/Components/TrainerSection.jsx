eac116268189685310d58251a243a4d0032aa86f
jeswanth <jachu.k1212@gmail.com>
2024-11-03 07:28:55 +0530

    Neogym

---
+++ neogym/src/Components/TrainerSection.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import trainer1 from "./path/to/images/t1.jpg";
+import trainer2 from "./path/to/images/t2.jpg";
+import trainer3 from "./path/to/images/t3.jpg";
+import facebookLogo from "./path/to/images/facebook-logo.png";
+import twitterLogo from "./path/to/images/twitter.png";
+import instagramLogo from "./path/to/images/instagram-logo.png";
+
+class TrainerSection extends Component {
+  render() {
+    return (
+      <section className="trainer_section layout_padding">
+        <div className="container">
+          <div className="heading_container">
+            <h2>Our Gym Trainers</h2>
+          </div>
+          <div className="row">
+            {/* Trainer 1 */}
+            <div className="col-lg-4 col-md-6 mx-auto">
+              <div className="box">
+                <div className="name">
+                  <h5>Smirth Jon</h5>
+                </div>
+                <div className="img-box">
+                  <img src={trainer1} alt="Smirth Jon" />
+                </div>
+                <div className="social_box">
+                  <a href="">
+                    <img src={facebookLogo} alt="Facebook" />
+                  </a>
+                  <a href="">
+                    <img src={twitterLogo} alt="Twitter" />
+                  </a>
+                  <a href="">
+                    <img src={instagramLogo} alt="Instagram" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* Trainer 2 */}
+            <div className="col-lg-4 col-md-6 mx-auto">
+              <div className="box">
+                <div className="name">
+                  <h5>Jean Doe</h5>
+                </div>
+                <div className="img-box">
+                  <img src={trainer2} alt="Jean Doe" />
+                </div>
+                <div className="social_box">
+                  <a href="">
+                    <img src={facebookLogo} alt="Facebook" />
+                  </a>
+                  <a href="">
+                    <img src={twitterLogo} alt="Twitter" />
+                  </a>
+                  <a href="">
+                    <img src={instagramLogo} alt="Instagram" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* Trainer 3 */}
+            <div className="col-lg-4 col-md-6 mx-auto">
+              <div className="box">
+                <div className="name">
+                  <h5>Alex Den</h5>
+                </div>
+                <div className="img-box">
+                  <img src={trainer3} alt="Alex Den" />
+                </div>
+                <div className="social_box">
+                  <a href="">
+                    <img src={facebookLogo} alt="Facebook" />
+                  </a>
+                  <a href="">
+                    <img src={twitterLogo} alt="Twitter" />
+                  </a>
+                  <a href="">
+                    <img src={instagramLogo} alt="Instagram" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default TrainerSection;
