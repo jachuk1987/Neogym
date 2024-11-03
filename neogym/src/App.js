00a304f1e9cb76e6ab4f026507f9de7d20f489ec
jeswanth <jachu.k1212@gmail.com>
2024-11-03 07:21:30 +0530

    Neogym

--- neogym/src/App.js
+++ neogym/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import HeaderSection from './Components/HeaderSection';
 import SliderSection from './Components/SliderSection';
+import UsSection from './Components/UsSection';
 
 function App() {
   return (
     <div className="App">
       <HeaderSection />
       <SliderSection />
+      <UsSection />
     </div>
   );
 }
